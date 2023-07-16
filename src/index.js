6aab68a1eb85313eea0f9e9a0784736e887f5ba8
maretabrownargus <maretabrown@gmail.com>
2023-07-16 13:09:49 +1000

    added timestamp function to JS, corrected max min temps

--- src/index.js
+++ src/index.js
@@ -24,29 +24,45 @@ function formateDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data;
+
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri"];
-
   let forecastHTML = `<div class= "row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  days.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
               <div class ="weather-forecast-date"> 
-             ${day}</div>
-          
-              <img src="src/clear-sky-day.png" alt="sunny day" width="35">
-          </br>
+             ${forecastDay.dt}
+             </div>
+              <img src=""http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                forecastDay.weather[0].icon
+              }" 
+              alt="" 
+              width="35">
           <div class="weather-forecast-temperature"> 
-            <span class="weather-forecast-temperature-max">18°</span>
-             <span class="weather-forecast-temperature-min">12°</span>
+            <span class="weather-forecast-temperature-max">${Math.round(
+              forecastDay.temp.max
+            )}°</span>
+             <span class="weather-forecast-temperature-min">${Math.round(
+               forecastDay.temp.min
+             )}°</span>
            </div>
             </div>
       `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -71,7 +87,6 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  getForecast(response.data.coordinates);
 
   celsiusTemperature = response.data.temperature.current;
 
@@ -123,8 +138,6 @@ function displayCelsiusTemperature(event) {
 let temperatureElement = document.querySelector("#temperature");
 
 let celsiusTemperature = null;
-
-displayForecast();
 
 search("Brisbane");
 
